759476813e2351e1227eb18497694c191f4c30ab
andrecoutoeusou <andrecouto.eusou@gmail.com>
2025-12-03 22:50:58 +0000

    Initialize quantum module package
    Add quantum module for ÉDEN Crypto framework

---
+++ eden_crypto/quantum/__init__.py
@@ -0,0 +1,8 @@
+"""ÉDEN Crypto - Quantum Module
+
+This module provides quantum computing integration for the ÉDEN cryptographic framework.
+"""
+
+from .hybrid import EDENQuantumHybrid
+
+__all__ = ['EDENQuantumHybrid']
